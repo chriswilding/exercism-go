efc44c1c03bc6cb13461caefdee30a1ebac5bea4
Chris Wilding <hello@chriswilding.co.uk>
2020-08-01 17:32:22 +0100

    Add hello world

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting implements the classic hello world program
+package greeting
+
+// HelloWorld returns the classic "Hello, World!"
+func HelloWorld() string {
+	return "Hello, World!"
+}
