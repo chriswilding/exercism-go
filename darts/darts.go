bfb413f39f3c9b8a3261f79053fe28bf2b2cba5b
Chris Wilding <hello@chriswilding.co.uk>
2020-08-05 20:47:08 +0100

    Update darts

--- darts/darts.go
+++ darts/darts.go
@@ -4,7 +4,7 @@ import "math"
 
 // Score calculates a darts score for given x and y coordinates.
 func Score(x, y float64) int {
-	radius := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+	radius := math.Hypot(x, y)
 	switch {
 	case radius <= 1.0:
 		return 10
