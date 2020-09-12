f18ad056d993dcd14ebb6e7d3d89a3f9c433b09b
Chris Wilding <hello@chriswilding.co.uk>
2020-09-12 18:27:11 +0100

    Update scale generator

--- scale-generator/scale_generator.go
+++ scale-generator/scale_generator.go
@@ -8,37 +8,40 @@ var flat = []string{"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "
 var sharp = []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
 var intervals = map[rune]int{'m': 1, 'M': 2, 'A': 3}
 
+func findScale(tonic string) []string {
+	switch tonic {
+	case "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb":
+		return flat
+	case "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#":
+		return sharp
+	default:
+		return sharp
+	}
+}
+
+func findIndexInScale(scale []string, tonic string) int {
+	for i, candidate := range scale {
+		if candidate == tonic {
+			return i
+		}
+	}
+	return 0
+}
+
 func Scale(tonic, interval string) []string {
 	if interval == "" {
 		interval = "mmmmmmmmmmmm"
 	}
 
-	var chromatic []string
-	switch tonic {
-	case "F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb":
-		chromatic = flat
-	case "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#":
-		chromatic = sharp
-	default:
-		chromatic = sharp
-	}
-
-	var chromaticScaleIndex int
-	chromaticScaleLookup := strings.Title(tonic)
-
-	for i, candidate := range chromatic {
-		if candidate == chromaticScaleLookup {
-			chromaticScaleIndex = i
-			break
-		}
-	}
+	chromatic := findScale(tonic)
+	chromaticScaleIndex := findIndexInScale(chromatic, strings.Title(tonic))
 
 	output := make([]string, 0, len(interval))
 
-	for _, il := range interval {
+	for _, i := range interval {
 		output = append(output, chromatic[chromaticScaleIndex])
-		in := intervals[il]
-		chromaticScaleIndex = chromaticScaleIndex + in
+		n := intervals[i]
+		chromaticScaleIndex += n
 		if chromaticScaleIndex >= len(chromatic) {
 			chromaticScaleIndex -= len(chromatic)
 		}
