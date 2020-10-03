151c7387c7ae721c5d7d367c274a238cdc1674ca
Chris Wilding <hello@chriswilding.co.uk>
2020-10-03 22:50:23 +0100

    Add perfect numbers

---
+++ perfect-numbers/perfect_numbers.go
@@ -0,0 +1,34 @@
+package perfect
+
+import "errors"
+
+type Classification int
+
+const (
+	ClassificationPerfect   Classification = iota
+	ClassificationAbundant  Classification = iota
+	ClassificationDeficient Classification = iota
+)
+
+var ErrOnlyPositive error = errors.New("must be a positive integer")
+
+func Classify(n int64) (Classification, error) {
+	if n < 1 {
+		return 0, ErrOnlyPositive
+	}
+	var sum int64
+	limit := n/2 + 1
+	for i := int64(1); i < limit; i++ {
+		if n%i == 0 {
+			sum += i
+		}
+	}
+	if sum == n {
+		return ClassificationPerfect, nil
+	}
+	if sum > n {
+		return ClassificationAbundant, nil
+	}
+	return ClassificationDeficient, nil
+
+}
