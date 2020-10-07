b6b912016a70530aa876e9104e12ac82d017e35b
Chris Wilding <hello@chriswilding.co.uk>
2020-10-07 21:36:25 +0100

    Update armstrong numbers

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,14 +1,19 @@
 package armstrong
 
-import "math"
+import (
+	"math"
+)
 
 func IsNumber(input int) bool {
-	length := int(math.Floor(math.Log10(float64(input)))) + 1
+	length := math.Floor(math.Log10(float64(input))) + 1
 
 	var sum int
-	for i := 0; i < length; i++ {
-		nth := input % int(math.Pow10(i+1)) / int(math.Pow10(i))
-		sum += int(math.Pow(float64(nth), float64(length)))
+	rem := input
+
+	for rem > 0 {
+		nth := float64(rem % 10)
+		rem /= 10
+		sum += int(math.Pow(nth, length))
 	}
 
 	return input == sum
