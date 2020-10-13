0de7a612ee6fcb5476ba5fd0e35b5f3521ed9c47
Chris Wilding <hello@chriswilding.co.uk>
2020-10-13 19:44:33 +0100

    Update flatten array

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -2,15 +2,16 @@ package flatten
 
 func Flatten(array interface{}) []interface{} {
 	output := []interface{}{}
+	return flatten(array, output)
+}
 
-	for _, value := range array.([]interface{}) {
-		switch value.(type) {
-		case []interface{}:
-			output = append(output, Flatten(value)...)
-		case nil:
-		default:
-			output = append(output, value)
+func flatten(input interface{}, output []interface{}) []interface{} {
+	if array, ok := input.([]interface{}); ok {
+		for _, element := range array {
+			output = flatten(element, output)
 		}
+	} else if input != nil {
+		output = append(output, input)
 	}
 	return output
 }
