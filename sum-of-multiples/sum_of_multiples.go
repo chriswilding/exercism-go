6de7d7266875e6676184dcc17d8c00e0b268e2d4
Chris Wilding <hello@chriswilding.co.uk>
2020-11-01 19:51:27 +0000

    Add sum of multiples

---
+++ sum-of-multiples/sum_of_multiples.go
@@ -0,0 +1,16 @@
+package summultiples
+
+func SumMultiples(limit int, divisors ...int) int {
+	var sum int
+
+	for i := 1; i < limit; i++ {
+		for _, divisor := range divisors {
+			if divisor != 0 && i%divisor == 0 {
+				sum += i
+				break
+			}
+		}
+	}
+
+	return sum
+}
